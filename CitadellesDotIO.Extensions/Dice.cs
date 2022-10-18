5f5dd224fb4afb1c5953bae6aba9265c3a24100f
Pierre SAILLARD <pierre.saillard@proton.me>
2022-10-18 10:35:31 +0200

    Removed unused directories

--- CitadellesDotIO.Extensions/Dice.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography;
-
-namespace CitadellesDotIO.Extensions
-{
-    public static class Dice
-    {
-        public static int Roll(int diceSize) => RandomNumberGenerator.GetInt32(0, diceSize);
-    }
-}
