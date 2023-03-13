562e5a49cc13f33cd51e2b7c0b65c3b546450321
Pierre Saillard <pierre.saillard@proton.me>
2023-03-13 23:15:46 +0100

    Messed with editor

--- CitadellesDotIO.Godot/node_2d.cs
+++ CitadellesDotIO.Godot/node_2d.cs
@@ -1,16 +1,36 @@
+using CitadellesDotIO.Client;
+using CitadellesDotIO.Engine.DTOs;
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 public partial class node_2d : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("CitadellesDotIO.Godot");
-	}
+    PlayerClient Client = new PlayerClient("https://localhost:7257", "Pierre");
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    Label DebuggerText;
+    Button CreateGameButton;
+    // Called when the node enters the scene tree for the first time.
+    public override async void _Ready()
+    {
+        this.DebuggerText = GetNode<Label>(nameof(DebuggerText));
+        this.CreateGameButton = GetNode<Button>(nameof(CreateGameButton));
+        CreateGameButton.Pressed += async () => { await Client.CreateGame("GameName"); };
+        await Client.StartLobbyConnection();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (this.Client != null
+        && this.DebuggerText != null)
+        {
+            StringBuilder builder = new();
+            builder.Append("LobbyConnection : ").AppendLine(Client.LobbyConnectionState.ToString());
+            builder.Append("Game : ").AppendLine(Client.GameConnectionState.ToString());
+            DebuggerText.Text = builder.ToString();            
+        }
+    }
 }
