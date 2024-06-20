8d5214597e6738d20848d5917e4049f885c3e3db
Pierre Saillard <pierre.saillard@proton.me>
2024-06-20 04:08:18 +0200

    Avant début retravail alignement/overflow des cartes

--- CitadellesDotIO.DeckAssembly/wwwroot/scripts/DragAndDrop.js
+++ CitadellesDotIO.DeckAssembly/wwwroot/scripts/DragAndDrop.js
@@ -18,15 +18,14 @@ function getUpperTopCoordinates(element) {
 
 function applyOnCardMove(event, dropzoneClassName, dragManager, blazorComponent) {
     const draggable = event.target;
-    const pickIndex = parseInt(draggable.dataset.index);
-    const draggablePosition = getUpperTopCoordinates(draggable);
-    const draggableMovementPosition = { x: event.dx, y: event.dy };
-    const sourceDropzone = event.target.closest(dropzoneClassName);
-    const pickSource = sourceDropzone.id;
-    const sourceNeighboursPositions = getUpperTopCoordinatesForList(sourceDropzone, 'pickPoolDraggable');
+    const sourceDropzone = draggable.closest(dropzoneClassName);
+    if (!sourceDropzone) {
+        console.log('no source dropzone, OnCardMove move will return early');
+        return;
+    }
     var targetNeighboursPositions = null;
     var dragHoverTarget = 0; // DragHoverTarget.None
-    var dropzone = document.querySelector('.drop-available');
+    const dropzone = document.querySelector('.drop-available');
     if (dropzone) {
         if (sourceDropzone != dropzone) {
             dragHoverTarget = 2; // DragHoverTarget.Target
@@ -34,33 +33,37 @@ function applyOnCardMove(event, dropzoneClassName, dragManager, blazorComponent)
         }
         else dragHoverTarget = 1; // DragHoverTarget.Self        
     }
-    const onDragMoveEventArgs = {
-        pickIndex: pickIndex,
-        pickSource: pickSource,
+    const args = {
+        pickIndex: parseInt(draggable.dataset.index),
+        pickSource: sourceDropzone.id,
         dragHoverTarget: dragHoverTarget,
-        draggablePosition: draggablePosition,
-        draggableMovementPosition: draggableMovementPosition,
-        sourceNeighboursPositions: sourceNeighboursPositions,
+        draggablePosition: getUpperTopCoordinates(draggable),
+        draggableMovementPosition: { x: event.dx, y: event.dy },
+        sourceNeighboursPositions: getUpperTopCoordinatesForList(sourceDropzone, 'pickPoolDraggable'),
         targetNeighboursPositions: targetNeighboursPositions
     }
-    blazorComponent.invokeMethod('OnDragMove', onDragMoveEventArgs);
+    blazorComponent.invokeMethod('OnDragMove', args);
 }
-async function pickPoolDraggables(className, dropzoneClassName, blazorComponent, dragManager) {
+async function setupPickPoolDraggables(className, dropzoneClassName, blazorComponent, dragManager) {
     interact(className).draggable({
         intertia: true,
         listeners: {
             move(event) {
-                console.log("dragmove");
+                //console.log("dragmove");
                 applyOnCardMove(event, dropzoneClassName, dragManager, blazorComponent);
             },
             start(event) {
-
+                console.log("dragstart");
             },
             end(event) {
-                console.log("dragend");
+                //console.log("dragend");
                 const draggable = event.target;
                 const pickIndex = parseInt(draggable.dataset.index);
                 const sourceDropzone = event.target.closest(dropzoneClassName);
+                if (!sourceDropzone) {
+                    console.log('no source dropzone, OnDragEnd will return early');
+                    return;
+                }
                 const pickSource = sourceDropzone.id;
                 const onDragEndEventArgs = {
                     pickIndex: pickIndex,
@@ -72,39 +75,46 @@ async function pickPoolDraggables(className, dropzoneClassName, blazorComponent,
     })
 };
 
-function pickPoolDropzones(className, blazorComponent) {
+function setupPickPoolDropzones(className, blazorComponent) {
     interact(className)
         .dropzone({
             ondragleave: function (event) {
-                var dropzone = event.target;
-                var draggable = event.relatedTarget;
-                var sourceDropzone = event.relatedTarget.closest(className);
+                const dropzone = event.target;
+                //const draggable = event.relatedTarget;
+                //const sourceDropzone = event.relatedTarget.closest(className);
                 //console.log(draggable.id + ' was moved out of ' + dropzone.id);
-                event.target.classList.remove('drop-available');
+                dropzone.classList.remove('drop-available');
             },
             ondragenter: function (event) {
-                var dropzone = event.target;
-                var draggable = event.relatedTarget;
-                var sourceDropzone = event.relatedTarget.closest(className);
+                const dropzone = event.target;
+                //var draggable = event.relatedTarget;
+                //var sourceDropzone = event.relatedTarget.closest(className);                
                 //console.log(draggable.id + ' was moved into ' + dropzone.id + ' from ' + sourceDropzone.id);
-
-                sourceDropzone.classList.remove('drop-available');
                 // Ajout d'une classe récupérée plus tard pour savoir si on hover
-                event.target.classList.add('drop-available');
+                dropzone.classList.add('drop-available');
             },
             ondrop: function (event) {
-                const dropzone = event.target;
+                //console.log('dropped');
                 const draggable = event.relatedTarget;
-                const pickIndex = parseInt(draggable.dataset.index);
                 const sourceDropzone = draggable.closest(className);
-                const pickSource = sourceDropzone.id;
+                if (!sourceDropzone) {
+                    console.log('no source dropzone, OnDrop will return early');
+                    return;
+                }
+                const dropzone = event.target;
+                dropzone.classList.remove('drop-available');               
                 //console.log(draggable.id + ' was dropped into ' + dropzone.id + ' from ' + sourceDropzone.id);
-
                 var dropEventSource = 1 // DropEventSource.Target
                 if (sourceDropzone == dropzone) {
                     dropEventSource = 0;// DropEventSource.Self
                 }
-                const args = { pickIndex: pickIndex, pickSource: pickSource, dropEventSource: dropEventSource, destination: dropzone.id };
+                const args =
+                {
+                    pickIndex: parseInt(draggable.dataset.index),
+                    pickSource: sourceDropzone.id,
+                    dropEventSource: dropEventSource,
+                    destination: dropzone.id
+                };
                 blazorComponent.invokeMethod('OnDrop', args);
             }
         })
